75d61e1e8ee2b31d4e6a640ccd36bafec8a869b8
Nevskiy911 <maleev1820@gmail.com>
2025-03-26 21:18:14 +0100

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import 'normalize.css';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import style from "./App.module.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <App className={style}/>
   </StrictMode>
 );
